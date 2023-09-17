7480a108e9010b99548cf1b0706c4a1055d4a252
sAndrin3 <sandrinemalanga2001@gmail.com>
2023-09-17 12:47:44 +0300

    Validation and Tostr

--- Models/BookDto.cs
+++ Models/BookDto.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
 namespace BookMgmt.Models{
     public class BookDto{
         public Guid Id {get; set;}
+        [Required(ErrorMessage = "Title is required.")]
         public string Title {get; set;} = string.Empty;
+
+        [Required(ErrorMessage = "Description is required.")]
         public string Description {get; set; } = string.Empty;
+        
+        [Required(ErrorMessage = "Author is required.")]
         public  List<AuthorDto> Author { get; set; } = new List<AuthorDto>();
         public DateTime CreatedAt { get; set; } = DateTime.Now;
     }
